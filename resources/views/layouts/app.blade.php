83c8e222a87d15da237c47ee2edbcc6fb4967ae2
mikkp17 <mikkeleigner@gmail.com>
2020-02-13 23:57:01 +0100

    Fixed last issues with env, removed tailwind, played around with bootstrap a little bit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -19,64 +20,21 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
+
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+    <!-- include Header layout -->
+    @include('layouts.header')
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+    <!-- container for displaying dynamic content -->
+    <div class="main-content">
+        <div class="container-xl">
             @yield('content')
-        </main>
+        </div>
     </div>
+    <!-- include Footer layout -->
     @include('layouts.footer')
+    <!-- Bootstrap JavaScript -->
     <script src="/js/app.js"></script>
 </body>
+
 </html>
--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,59 +1,21 @@
-<section class="bg-white py-8 w-full">
-    <div class="container mx-auto px-8">
-        <div class="table w-full">
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Links</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">FAQ</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Help</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="/support" class="text-grey hover:text-grey-dark">Support</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Legal</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Terms</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Privacy</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Social</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Facebook</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Linkedin</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Twitter</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="block sm:table-cell">
-                <p class="uppercase text-grey text-sm sm:mb-6">Company</p>
-                <ul class="list-reset text-xs mb-6">
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Official Blog</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">About Us</a>
-                    </li>
-                    <li class="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="#" class="text-grey hover:text-grey-dark">Contact</a>
-                    </li>
-                </ul>
-            </div>
+<div class="w-100 text-light bg-secondary">
+    <div class="container-xl d-flex justify-content-between">
+        <div>
+            (Icons)
+        </div>
+        <div>
+            <a href="#" class="text-light">Column one</a>
+        </div>
+        <div>
+            <a href="#" class="text-light">Column two</a>
+        </div>
+        <div>
+            <a href="#" class="text-light">Column three</a>
+            <br>
+            <a href="#" class="text-light">Login</a>
+        </div>
+        <div>
+            (Icons)
         </div>
     </div>
-</section>
+</div>
