ea6d596cbdaaa80c50d8bcf1698a7c4be807bd7a
Albert Johansson <albertjohansson1994@live.se>
2020-02-14 20:02:17 +0100

    asd123

--- resources/lang/sv/auth.php
+++ resources/lang/sv/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Den information du har angett stämmer inte.',
+    'throttle' => 'För många försök. Försök igen om :seconds sekunder.',
 
 ];
