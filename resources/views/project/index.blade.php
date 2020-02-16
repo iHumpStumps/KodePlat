35abb7b8056ef8470a3f3292db6ead074c3f6796
Albert Johansson <albertjohansson1994@live.se>
2020-02-16 16:50:04 +0100

    fixed footer etc

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -2,27 +2,32 @@
 
 @section('content')
     <div class="card-deck">
-    @foreach($projects as $project)
-        <div
-            class="card"
-            style="width: 18rem;"
-        >
-            <img
-                src="storage/{{ $images['image' . $project->id] }}"
-                class="card-img-top"
-                alt="..."
+        @foreach($projects as $project)
+            <div
+                class="card"
+                style="width: 18rem;"
             >
-            <div class="card-body" style="text-align: center">
-                <h5 class="card-title">{{ $project->title }}</h5>
-                <p class="card-text">{{ $project->information }}</p>
-                <a
-                    href="{{ route('projects.show', $project) }}"
-                    class="btn btn-primary"
+                <div>
+                    <img
+                        src="storage/{{ $images['image' . $project->id] }}"
+                        class="card-img-top"
+                        alt="..."
+                    />
+                </div>
+                <div
+                    class="card-body"
+                    style="text-align: center"
                 >
-                    {{ __('more') }}
-                </a>
+                    <h5 class="card-title">{{ $project->title }}</h5>
+                    <p class="card-text">{{ $project->information }}</p>
+                    <a
+                        href="{{ route('projects.show', $project) }}"
+                        class="btn btn-primary"
+                    >
+                        {{ __('more') }}
+                    </a>
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
     </div>
 @endsection
