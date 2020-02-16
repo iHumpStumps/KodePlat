ac974ae8073d8b011b3dafea97e76e315e00691a
mikkp17 <mikkeleigner@gmail.com>
2020-02-16 16:55:04 +0100

    Update index.blade.php

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -4,38 +4,39 @@
     <div class="card-deck row-cols-1 row-cols-md-3">
         @foreach($projects as $project)
             <div class="col mb-3">
-                <a href=""
-                <div
-                    class="card h-100"
-                    style="max-width: 24rem;"
-                >
-                    <div class="HoverDiv">
-                        <img
-                            src="storage/{{ $images['image' . $project->id] }}"
-                            class="card-img-top"
-                            alt="..."
-                        >
-                    </div>
-
+                <a href="/" class="">
                     <div
-                        class="card-body"
-                        style="text-align: center; background-color: white"
+                        class="card h-100"
+                        style="max-width: 24rem;"
                     >
-                        <h5 class="card-title">
-                            {{ $project->title }}
-                        </h5>
-                        <p class="card-text">
-                            {{ $project->information }}
-                        </p>
-                    </div>
-                    <div class="card-footer text-muted text-center">
+                        <div class="HoverDiv">
+                            <img
+                                src="storage/{{ $images['image' . $project->id] }}"
+                                class="card-img-top"
+                                alt="..."
+                            >
+                        </div>
+
+                        <div
+                            class="card-body"
+                            style="text-align: center; background-color: white"
+                        >
+                            <h5 class="card-title">
+                                {{ $project->title }}
+                            </h5>
+                            <p class="card-text">
+                                {{ $project->information }}
+                            </p>
+                        </div>
+                        <div class="card-footer text-muted text-center">
                         {{ '#' . 'Placeholder' }} <!-- Placeholder for tags such as punchjob, etc. -->
-                        {{ '#' . $project->metal }}
-                        <div class="float-right">
-                            {{ $project->year }}
+                            {{ '#' . $project->metal }}
+                            <div class="float-right">
+                                {{ $project->year }}
+                            </div>
                         </div>
                     </div>
-                </div>
+                </a>
             </div>
         @endforeach
     </div>
